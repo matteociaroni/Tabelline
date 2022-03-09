04197a1ff796abecd00b57cae350a35f749a06e0
matteociaroni <matteo.ciaroni@virgilio.it>
2022-03-09 22:56:23 +0100

    Rimosse variabili globali JS

--- script.js
+++ script.js
@@ -1,29 +1,53 @@
-let num1, num2;
-let tabellineSelezionate;
+function getNum1()
+{
+	return localStorage.getItem("num1");
+}
+
+function getNum2()
+{
+	return localStorage.getItem("num2");
+}
+
+function getTabellineSelezionate()
+{
+	const selected=localStorage.getItem("tabellineSelezionate");
+	if(selected!=null)
+		return selected.split(",");
+	else
+		return Array();
+}
+
+function setNum1(value)
+{
+	localStorage.setItem("num1", value);
+}
+
+function setNum2(value)
+{
+	localStorage.setItem("num2", value);
+}
 
 function init(random=true)
 {
-	if(random)
+	if(random || getNum1()==null || getNum2()==null)
 	{
-		num1=getRandomInt(true).toString();
-		num2=getRandomInt(false).toString();
+		setNum1(getRandomInt(true))
+		setNum2(getRandomInt(false))
 	}
 
-	document.getElementById("num1").innerHTML=num1;
-	document.getElementById("num2").innerHTML=num2;
+	document.getElementById("num1").innerHTML=getNum1().toString();
+	document.getElementById("num2").innerHTML=getNum2().toString();
 	document.getElementById("result").innerHTML="";
 	document.getElementById("inserito").value="";
 	document.getElementById("inserito").disabled="";
 	document.getElementById("init-button").style.display="none";
-	localStorage.setItem("num1", num1);
-	localStorage.setItem("num2", num2);
 }
 
 function checkResult()
 {
 	const inserito=parseInt(document.getElementById("inserito").value);
 
-	if(inserito===num1*num2)
+	if(inserito===getNum1()*getNum2())
 	{
 		document.getElementById("result").innerHTML="😊";
 		document.getElementById("inserito").disabled="true";
@@ -35,65 +59,35 @@ function checkResult()
 
 function getRandomInt(check=false)
 {
-	const tabelline=localStorage.getItem("tabellineSelezionate");
-	if(tabelline!=null)
-		tabellineSelezionate=tabelline.split(",");
-
 	let n;
-	if(check && tabellineSelezionate.length>1)
-		n=tabellineSelezionate[Math.floor(Math.random()*(tabellineSelezionate.length-1))];
+	if(check && getTabellineSelezionate().length>1)
+		n=getTabellineSelezionate()[Math.floor(Math.random()*(getTabellineSelezionate().length-1))];
 	else
 		n=Math.floor(Math.random()*11);
 
 	return n;
 }
 
-function restore()
-{
-	const oldNum1=localStorage.getItem("num1");
-	const oldNum2=localStorage.getItem("num2");
-	const tabelline=localStorage.getItem("tabellineSelezionate");
-	if(tabelline!=null)
-		tabellineSelezionate=tabelline.split(",");
-
-	if(oldNum1!=null && oldNum2!=null)
-	{
-		num1=oldNum1;
-		num2=oldNum2;
-		init(false);
-	}
-	else
-		init(true);
-}
-
 function updateSelected()
 {
 	let inputs=document.getElementsByTagName("input");
-	tabellineSelezionate=Array();
+	let tabellineSelezionate=Array();
 	for(let i=0; i<inputs.length; i++)
 	{
 		if(inputs.item(i).checked===true)
-		{
 			tabellineSelezionate.push(inputs.item(i).id);
-		}
 	}
 	localStorage.setItem("tabellineSelezionate", tabellineSelezionate);
-	if(tabellineSelezionate.indexOf(localStorage.getItem("num1"))<0)
+	if(tabellineSelezionate.indexOf(getNum1())<0)
 		localStorage.removeItem("num1");
 }
 
 function setSelected()
 {
-	const tabelline=localStorage.getItem("tabellineSelezionate");
-	if(tabelline!=null)
-		tabellineSelezionate=tabelline;
-
 	let inputs=document.getElementsByTagName("input");
 	for(let i=0; i<inputs.length; i++)
 	{
-		if(tabellineSelezionate.indexOf(inputs.item(i).id)>-1)
-		{
+		if(getTabellineSelezionate().indexOf(inputs.item(i).id)>-1)
 			inputs.item(i).checked=true;
-		}
 	}
 }
