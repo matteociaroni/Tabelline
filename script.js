69f3a2e81997e864ebb2a7d9c50483b624444a59
matteociaroni <matteo.ciaroni@virgilio.it>
2022-05-01 10:34:21 +0200

    Introdotta programmazione ad oggetti

--- script.js
+++ script.js
@@ -1,67 +1,79 @@
-function getNum1() {
-    return parseInt(localStorage.getItem("num1"));
-}
-function getNum2() {
-    return parseInt(localStorage.getItem("num2"));
-}
-function getTabellineSelezionate() {
-    const selected = localStorage.getItem("tabellineSelezionate");
-    if (selected != null)
-        return selected.split(",").map(Number);
-    else
-        return Array();
-}
-function setNum1(value) {
-    localStorage.setItem("num1", value.toString());
-}
-function setNum2(value) {
-    localStorage.setItem("num2", value.toString());
-}
-function init(random = true) {
-    if (random || isNaN(getNum1()) || isNaN(getNum2())) {
-        setNum1(getRandomInt(true));
-        setNum2(getRandomInt(false));
+class Num {
+    id;
+    constructor(id) {
+        this.id = id;
     }
-    document.getElementById("num1").innerHTML = getNum1().toString();
-    document.getElementById("num2").innerHTML = getNum2().toString();
-    document.getElementById("result").innerHTML = "";
-    document.getElementById("inserito").value = "";
-    document.getElementById("inserito").removeAttribute("disabled");
-    document.getElementById("init-button").style.display = "none";
-}
-function checkResult() {
-    const inserito = parseInt(document.getElementById("inserito").value);
-    if (inserito == getNum1() * getNum2()) {
-        document.getElementById("result").innerHTML = "😊";
-        document.getElementById("inserito").setAttribute("disabled", "true");
-        document.getElementById("init-button").style.display = "";
+    getValue() {
+        return parseInt(localStorage.getItem(this.id));
     }
-    else if (!isNaN(inserito))
-        document.getElementById("result").innerHTML = "😞";
-}
-function getRandomInt(check = false) {
-    let n;
-    if (check && getTabellineSelezionate().length > 0)
-        n = getTabellineSelezionate()[Math.round(Math.random() * (getTabellineSelezionate().length - 1))];
-    else
-        n = Math.floor(Math.random() * 11);
-    return n;
-}
-function updateSelected() {
-    let inputs = document.getElementsByTagName("input");
-    let tabellineSelezionate = Array();
-    for (let i = 0; i < inputs.length; i++) {
-        if (inputs.item(i).checked === true)
-            tabellineSelezionate.push(inputs.item(i).id);
-    }
-    localStorage.setItem("tabellineSelezionate", tabellineSelezionate.toString());
-    if (tabellineSelezionate.indexOf(getNum1()) < 0)
-        localStorage.removeItem("num1");
-}
-function setSelected() {
-    let inputs = document.getElementsByTagName("input");
-    for (let i = 0; i < inputs.length; i++) {
-        if (getTabellineSelezionate().indexOf(parseInt(inputs.item(i).id)) > -1)
-            inputs.item(i).checked = true;
+    setValue(value) {
+        localStorage.setItem(this.id, value.toString());
     }
 }
+class Game {
+    num1;
+    num2;
+    constructor() {
+        this.num1 = new Num("num1");
+        this.num2 = new Num("num2");
+    }
+    getTabellineSelezionate() {
+        const selected = localStorage.getItem("tabellineSelezionate");
+        if (selected != null)
+            return selected.split(",").map(Number);
+        else
+            return Array();
+    }
+    setNumbers(random) {
+        if (random || isNaN(this.num1.getValue()) || isNaN(this.num2.getValue())) {
+            this.num1.setValue(this.getRandomInt(true));
+            this.num2.setValue(this.getRandomInt(false));
+        }
+    }
+    init(random = true) {
+        this.setNumbers(random);
+        document.getElementById("num1").innerHTML = this.num1.getValue().toString();
+        document.getElementById("num2").innerHTML = this.num2.getValue().toString();
+        document.getElementById("result").innerHTML = "";
+        document.getElementById("inserito").value = "";
+        document.getElementById("inserito").removeAttribute("disabled");
+        document.getElementById("init-button").style.display = "none";
+    }
+    checkResult() {
+        const inserito = parseInt(document.getElementById("inserito").value);
+        if (inserito == this.num1.getValue() * this.num2.getValue()) {
+            document.getElementById("result").innerHTML = "😊";
+            document.getElementById("inserito").setAttribute("disabled", "true");
+            document.getElementById("init-button").style.display = "";
+        }
+        else if (!isNaN(inserito))
+            document.getElementById("result").innerHTML = "😞";
+    }
+    getRandomInt(check = false) {
+        let n;
+        if (check && this.getTabellineSelezionate().length > 0)
+            n = this.getTabellineSelezionate()[Math.round(Math.random() * (this.getTabellineSelezionate().length - 1))];
+        else
+            n = Math.floor(Math.random() * 11);
+        return n;
+    }
+    changeTabelline() {
+        let inputs = document.getElementsByTagName("input");
+        let tabellineSelezionate = Array();
+        for (let i = 0; i < inputs.length; i++) {
+            if (inputs.item(i).checked === true)
+                tabellineSelezionate.push(inputs.item(i).id);
+        }
+        localStorage.setItem("tabellineSelezionate", tabellineSelezionate.toString());
+        if (tabellineSelezionate.indexOf(this.num1.getValue()) < 0)
+            localStorage.removeItem("num1");
+    }
+    setTabellineCheckboxes() {
+        let inputs = document.getElementsByTagName("input");
+        for (let i = 0; i < inputs.length; i++) {
+            if (this.getTabellineSelezionate().indexOf(parseInt(inputs.item(i).id)) > -1)
+                inputs.item(i).checked = true;
+        }
+    }
+}
+const g = new Game();
--- script.ts
+++ script.ts
@@ -1,95 +1,114 @@
-function getNum1() : number
+class Num
 {
-	return parseInt(localStorage.getItem("num1"));
-}
+	private id : string
 
-function getNum2() : number
-{
-	return parseInt(localStorage.getItem("num2"));
-}
-
-function getTabellineSelezionate() : number[]
-{
-	const selected = localStorage.getItem("tabellineSelezionate");
-	if(selected != null)
-		return selected.split(",").map(Number);
-	else
-		return Array();
-}
-
-function setNum1(value : number) : void
-{
-	localStorage.setItem("num1", value.toString());
-}
-
-function setNum2(value : number) : void
-{
-	localStorage.setItem("num2", value.toString());
-}
-
-function init(random : boolean = true) : void
-{
-	if(random || isNaN(getNum1()) || isNaN(getNum2()))
+	constructor(id : string)
 	{
-		setNum1(getRandomInt(true));
-		setNum2(getRandomInt(false));
+		this.id=id
 	}
 
-	document.getElementById("num1").innerHTML = getNum1().toString();
-	document.getElementById("num2").innerHTML = getNum2().toString();
-	document.getElementById("result").innerHTML = "";
-	(<HTMLInputElement>document.getElementById("inserito")).value = "";
-	document.getElementById("inserito").removeAttribute("disabled");
-	document.getElementById("init-button").style.display = "none";
-}
-
-function checkResult() : void
-{
-	const inserito : number = parseInt((<HTMLInputElement>document.getElementById("inserito")).value);
-
-	if(inserito == getNum1() * getNum2())
+	getValue() : number
 	{
-		document.getElementById("result").innerHTML = "😊";
-		document.getElementById("inserito").setAttribute("disabled", "true");
-		document.getElementById("init-button").style.display = "";
+		return parseInt(localStorage.getItem(this.id));
 	}
-	else if(!isNaN(inserito))
-		document.getElementById("result").innerHTML = "😞";
-}
 
-function getRandomInt(check : boolean = false) : number
-{
-	let n : number;
-
-	if(check && getTabellineSelezionate().length > 0)
-		n = getTabellineSelezionate()[Math.round(Math.random() * (getTabellineSelezionate().length - 1))];
-	else
-		n = Math.floor(Math.random() * 11);
-
-	return n;
-}
-
-function updateSelected() : void
-{
-	let inputs = document.getElementsByTagName("input");
-	let tabellineSelezionate = Array();
-	for (let i = 0; i < inputs.length; i++)
+	setValue(value : number) : void
 	{
-		if(inputs.item(i).checked === true)
-			tabellineSelezionate.push(inputs.item(i).id);
-	}
-	localStorage.setItem("tabellineSelezionate", tabellineSelezionate.toString());
-
-	if(tabellineSelezionate.indexOf(getNum1()) < 0)
-		localStorage.removeItem("num1");
-}
-
-function setSelected() : void
-{
-	let inputs = document.getElementsByTagName("input");
-	for (let i = 0; i < inputs.length; i++)
-	{
-		if(getTabellineSelezionate().indexOf(parseInt(inputs.item(i).id)) > -1)
-			inputs.item(i).checked = true;
+		localStorage.setItem(this.id, value.toString());
 	}
 }
+
+class Game
+{
+	private num1 : Num
+	private num2 : Num
+
+	constructor()
+	{
+		this.num1=new Num("num1")
+		this.num2=new Num("num2")
+	}
+
+	getTabellineSelezionate() : number[]
+	{
+		const selected = localStorage.getItem("tabellineSelezionate");
+		if(selected != null)
+			return selected.split(",").map(Number);
+		else
+			return Array();
+	}
+
+	setNumbers(random : boolean) : void
+	{
+		if(random || isNaN(this.num1.getValue()) || isNaN(this.num2.getValue()))
+		{
+			this.num1.setValue(this.getRandomInt(true));
+			this.num2.setValue(this.getRandomInt(false));
+		}
+	}
+
+	init(random : boolean = true) : void
+	{
+		this.setNumbers(random);
+
+		document.getElementById("num1").innerHTML = this.num1.getValue().toString();
+		document.getElementById("num2").innerHTML = this.num2.getValue().toString();
+		document.getElementById("result").innerHTML = "";
+		(<HTMLInputElement>document.getElementById("inserito")).value = "";
+		document.getElementById("inserito").removeAttribute("disabled");
+		document.getElementById("init-button").style.display = "none";
+	}
+
+	checkResult() : void
+	{
+		const inserito : number = parseInt((<HTMLInputElement>document.getElementById("inserito")).value);
+
+		if(inserito == this.num1.getValue() * this.num2.getValue())
+		{
+			document.getElementById("result").innerHTML = "😊";
+			document.getElementById("inserito").setAttribute("disabled", "true");
+			document.getElementById("init-button").style.display = "";
+		}
+		else if(!isNaN(inserito))
+			document.getElementById("result").innerHTML = "😞";
+	}
+
+	getRandomInt(check : boolean = false) : number
+	{
+		let n : number;
+
+		if(check && this.getTabellineSelezionate().length > 0)
+			n = this.getTabellineSelezionate()[Math.round(Math.random() * (this.getTabellineSelezionate().length - 1))];
+		else
+			n = Math.floor(Math.random() * 11);
+
+		return n;
+	}
+
+	changeTabelline() : void
+	{
+		let inputs = document.getElementsByTagName("input");
+		let tabellineSelezionate = Array();
+		for (let i = 0; i < inputs.length; i++)
+		{
+			if(inputs.item(i).checked === true)
+				tabellineSelezionate.push(inputs.item(i).id);
+		}
+		localStorage.setItem("tabellineSelezionate", tabellineSelezionate.toString());
+
+		if(tabellineSelezionate.indexOf(this.num1.getValue()) < 0)
+			localStorage.removeItem("num1");
+	}
+
+	setTabellineCheckboxes() : void
+	{
+		let inputs = document.getElementsByTagName("input");
+		for (let i = 0; i < inputs.length; i++)
+		{
+			if(this.getTabellineSelezionate().indexOf(parseInt(inputs.item(i).id)) > -1)
+				inputs.item(i).checked = true;
+		}
+	}
+}
+
+const g=new Game();
